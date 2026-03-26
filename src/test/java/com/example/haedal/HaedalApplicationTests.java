c2a1fab79f7ef92a6e5cec7b907901e85643a57e
maywindabnew <conan5125@naver.com>
2026-03-26 19:08:34 +0900

    first commit

---
+++ src/test/java/com/example/haedal/HaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
